2c5e25e0339fbe5949c496bb2e3b01aa5e62ccee
Vipul Gupta <mail.vipulg@gmail.com>
2023-12-16 19:16:02 +0530

    added required code

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,22 @@
 import React from 'react';
 
 import MyComponent from '../components/MyComponent';
+import { UserProvider } from '@/contexts/UserContext';
+import { ThemeProvider } from '@/contexts/ThemeContext';
+import { LanguageProvider } from '@/contexts/LanguageContext';
+import { ConfigProvider } from '@/contexts/ConfigContext';
 
 function App() {
   return (
+    <UserProvider>
+      <ThemeProvider>
+        <LanguageProvider>
+          <ConfigProvider>
             <MyComponent />
+          </ConfigProvider>
+        </LanguageProvider>
+      </ThemeProvider>
+    </UserProvider>
   );
 }
 
